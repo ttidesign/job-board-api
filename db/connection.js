6d0519d42b9e11c73efeebcc2a2717e58f981b41
ttdesign <tkthedesigner@gmail.com>
2020-05-15 08:55:31 -0700

    setup server

---
+++ db/connection.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const mongoURI =
+	process.env.NODE_ENV === 'production'
+		? process.env.MONGODB_URI
+		: 'mongodb://localhost/job-board';
+mongoose
+	.connect(mongoURI, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+	})
+	.then((instance) =>
+		console.log(`Connected to db: ${instance.connections[0].name}`)
+	)
+	.catch((error) => console.log('Connection failed!', error));
+
+module.exports = mongoose;
